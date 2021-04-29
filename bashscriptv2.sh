c62c9b2dfcea2e9bb2e8709f2f862c2a4debaf48
cmiranda89 <carlosjrmiranda3@gmail.com>
2021-04-29 00:11:45 -0500

    Script

---
+++ bashscriptv2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo -n "Current User:cmiranda09@localhost"
+echo -n "Intel(R) Core(TM) i5-3210M CPU @ 2.50GHZ"
+echo -n "(Free:86) (Total:1768)"
+echo -n "(Free:86) (Swap:2047)"
+echo -n "(IP: 192.168.122.1) (Subnet: 255.255.255.0)"
+
